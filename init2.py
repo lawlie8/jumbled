f907586e1debb71bb273ff4b72d69fc75357fc05
lawlie8 <lawlie8@pm.me>
2019-08-01 20:01:47 +0530

    made it real good:)

--- init2.py
+++ init2.py
@@ -8,7 +8,7 @@ def check(get,list,x,hell):
             pos1 = lines[pos]
             pos2 = lines[pos+1]
             hell = hell[pos1:pos2]
-            print(pos1,pos2)
+            
             for yell in hell:
                 if yell == get:
                     print(yell,time.strftime('%H:%M:%S'))
@@ -17,7 +17,7 @@ y = open('output\\sortlist.txt')
 hell = x.readlines()
 there = y.readlines()
 list  = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-lines = [1, 25418, 43831, 75937, 94670, 108867, 120760, 131713, 145456, 159798, 162638, 166590, 176592, 196397, 209855, 222536, 257396, 259189, 275972, 314736, 333555, 356322, 361651, 368210,368717]
+lines = [1, 25418, 43831, 75937, 94670, 108867, 120760, 131713, 145456,158655,161495,165447,175449,195254,208712,221393,256253,258046,274829,313593,332412,355179,360508,367067,367574,368717]
 for get in there:
     check(get,list,x,hell)
 print('Process completed at',time.strftime('%H:%M:%S'))
